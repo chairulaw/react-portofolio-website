bd86b857b8630353f4371bb280e19224829554c9
remainsguy <chairulaw>
2025-02-09 17:31:22 +0700

    fixed 1

--- src/assets/Assets.jsx
+++ src/assets/Assets.jsx
@@ -1,16 +1,15 @@
-// Image exports
-export { default as bengkelCover } from "./img/bengkel.jpg"
-export { default as comingSoonCover } from "./img/coming-soon.jpg" // Fixed space in filename
-export { default as hmsiProjectCover } from "./img/HMSI.jpg"
-export { default as imgProfile } from "./img/image-125.jpg" // Fixed space in filename
-export { default as originalProfile } from "./img/original.png"
-export { default as technofestCover } from "./img/technofest.jpg" // Fixed capitalization
-export { default as technoshiftCover } from "./img/technoshift-2.jpg" // Fixed space in filename
-export { default as webCoffeeShopCover } from "./img/web-project.jpg" // Fixed space in filename
-export { default as petaniGoCover } from "./img/petani-go.jpg" // Fixed space in filename
-export { default as disneyCloneCover } from "./img/image.png"
+export {default as bengkelCover} from './img/bengkel.jpg'
+export {default as comingSoonCover} from './img/coming soon.jpg'
+export {default as hmsiProjectCover} from './img/HMSI.jpg'
+export {default as imgProfile} from './img/image 125.jpg'
+export {default as originalProfile} from './img/original.png'
+export {default as technofestCover} from './img/Technofest.jpg'
+export {default as technoshiftCover} from './img/Technoshift 2.jpg'
+export {default as webCoffeeShopCover} from './img/web project.jpg'
+export {default as petaniGoCover} from './img/petani go.jpg'
+export {default as disneyCloneCover} from './img/image.png'
 
-// PDF exports
-export { default as cvEnglish } from "./PDF/CV-Muhammad-Chairul-Wibisono_English.pdf" // Fixed spaces in filename
-export { default as cvIndo } from "./PDF/CV-Muhammad-Chairul-Wibisono_Indonesia.pdf" // Fixed spaces in filename
 
+// PDF
+export {default as cvEnglish} from './PDF/CV Muhammad Chairul Wibisono_English.pdf'
+export {default as cvIndo} from './PDF/CV Muhammad Chairul Wibisono_Indonesia.pdf'
