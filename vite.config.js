36a1acbd98b6712f6ead276cec21d5fdd17e0ea2
remainsguy <arulwibisono155@gmail.com>
2025-02-24 12:01:36 +0700

    fix bugs 1.1

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/your-base-url/',
   plugins: [react()],
   build: {
     outDir: 'dist',
